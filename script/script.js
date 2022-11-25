5e01524c90207533d156128db0e6433a53d68470
Angelo Barbarulo <angelobarbarulo8@gmail.com>
2022-11-25 18:37:05 +0100

    clean the script

--- script/script.js
+++ script/script.js
@@ -6,13 +6,42 @@ const password = document.getElementById('pw');
 const submitBtn = document.getElementById('subm');
 const formSpace = document.querySelectorAll('.form-space');
 
-
+let expReg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
 
 //It doesn't show the 'error' icons at the beginning
 formSpace.forEach( el => {
     el.style.background = 'none';
 });
 
+
+const invalidInput = (name, error) => {
+    error.style.display = 'block';
+    name.style.border = '1px solid red';
+    name.style.background = '';
+}
+
+const validInput = (name, error) => {
+    error.style.display = '';
+    name.style.border = '1px solid var(--green)';
+    name.style.background = 'none';
+} 
+
+const validateEmail = () => {
+    let emValidate = expReg.test(email.value);
+    if(emValidate === false){
+        invalidInput(email, errMail);
+    } else {
+        validInput(email, errMail);
+    }
+}
+
+const validatePassword = () => {
+    if(password.value < 12){
+        invalidInput(password, errPass);
+    } else {
+        validInput(password, errPass);
+    }
+}
 
 
 const validateForm = () => {
@@ -21,16 +50,13 @@ const validateForm = () => {
     } else {
         validInput(firstName, errFName);
     }
-
     if(lastName.value === ''){
         invalidInput(lastName, errLName);
     } else {
         validInput(lastName, errLName);
     }
-
     validateEmail();
     validatePassword();
-    
 }
 
 submitBtn.addEventListener('click', event => {
@@ -40,38 +66,7 @@ submitBtn.addEventListener('click', event => {
 
 
 
-const invalidInput = (name, error) => {
-    error.style.display = 'block';
-    name.style.border = '1px solid red';
-    name.style.background = '';
-}
 
-
-const validInput = (name, error) => {
-    error.style.display = '';
-    name.style.border = '1px solid var(--green)';
-    name.style.background = 'none';
-} 
-
-
-const validateEmail = () => {
-    let expReg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-    let emValidate = expReg.test(email.value);
-    if(emValidate === false){
-        invalidInput(email, errMail);
-    } else {
-        validInput(email, errMail);
-    }
-}
-
-
-const validatePassword = () => {
-    if(password.value < 12){
-        invalidInput(password, errPass);
-    } else {
-        validInput(password, errPass);
-    }
-}
 
 
 
